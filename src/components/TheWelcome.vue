ee6c61a57bab50b276dcf3509bda94dd6a95c378
Zexi. Li <lzx12138@sjtu.edu.cn>
2023-11-15 15:30:23 +0800

    feat: moved files from vuesume.

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,88 +1,122 @@
-<script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
+<!-- 整体的框架 -->
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+  <a-layout>
+      <!-- 首屏 -->
+      <!-- <a-layout-header class="layout-header"><Banner/></a-layout-header> -->
+      <!-- 内容 -->
+      <!-- <a-layout> -->
+          <!-- 菜单 -->
+          <!-- <a-layout-sider class="layout-sider" width="320"><a-affix><Menu/></a-affix></a-layout-sider> -->
+          <!-- 正文部分 -->
+          <!-- <a-layout class="layout-content"> -->
+              <!-- 小屏侧边栏抽屉按钮 -->
+              <!-- <a-affix> -->
+                  <!-- <a-button :class="{'sider-menu-trigger': true, 'drawer-closed': !menuDrawerVisible, 'drawer-open': menuDrawerVisible}" -->
+                          <!-- shape="circle" size="large" :icon="menuDrawerVisible ? 'arrow-left' : 'bars'" @click="toggleMenuDrawer"></a-button> -->
+              <!-- </a-affix> -->
+              <!-- 正文锚点 -->
+              <!-- <a-layout-content><div id="anchor-next"></div></a-layout-content> -->
+              <!-- 根据配置动态模块的内容和顺序 -->
+              <!-- <a-layout-content v-for="id in moduleIds" v-bind:key="id"> -->
+                  <!-- <About v-if="id === 'about'"/> -->
+                  <!-- <Blog v-if="id === 'blog'"/> -->
+                  <!-- <Experience v-if="id === 'experience'"/> -->
+              <!-- </a-layout-content> -->
+              <!-- 页脚 -->
+              <!-- <a-layout-footer><Footer/></a-layout-footer> -->
+          <!-- </a-layout> -->
+      <!-- </a-layout> -->
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+      <!-- 小屏侧边栏抽屉 -->
+      <!-- <a-drawer placement="left" :closable="true" :visible="menuDrawerVisible" @close="onMenuDrawerClose"> -->
+      <!--     <Menu @menuClick="onMenuDrawerClose" /> -->
+      <!-- </a-drawer> -->
+  </a-layout>
 </template>
+
+<script lang="ts">
+  // import {Component, Vue} from 'vue-property-decorator';
+  // import {mapGetters} from 'vuex';
+
+  import Banner from '@/components/icons/Banner.vue';
+  import Menu from '@/components/icons/Menu.vue';
+  import About from '@/components/icons/About.vue';
+  import Experience from '@/components/icons/Experience.vue';
+  import Blog from '@/components/icons/Blog.vue';
+  import Footer from '@/components/icons/Footer.vue';
+
+  // @Component({
+      // components: {
+          // Banner,
+          // Menu,
+          // About,
+          // Experience,
+          // Blog,
+          // Footer,
+      // },
+      // computed: {
+          // ...mapGetters(['moduleIds']),
+      // },
+  // })
+  // export default class Home extends Vue {
+  export default class Home {
+      private menuDrawerVisible = false;
+
+      private toggleMenuDrawer() {
+          this.menuDrawerVisible = !this.menuDrawerVisible;
+      }
+
+      private onMenuDrawerClose() {
+          this.menuDrawerVisible = false;
+      }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import '../styles/variable';
+
+  .layout-header {
+      z-index: 0;
+      height: 100vh;
+      overflow: auto;
+      padding: 0;
+  }
+
+  .layout-content {
+      position: relative;
+  }
+
+  .sider-menu-trigger {
+      position: absolute;
+      top: 20px;
+      z-index: 99999;
+
+
+      &.drawer-closed {
+          left: 20px;
+      }
+
+      &.drawer-open {
+          left: 276px;
+      }
+  }
+
+  @media screen and (max-width: $--screen-sm-min) {
+      .layout-sider {
+          display: none;
+      }
+  }
+
+  @media screen and (min-width: $--screen-sm-min) {
+      .sider-menu-trigger {
+          display: none;
+      }
+
+      .layout-sider {
+          box-shadow: 1px 0 5px #e0e0e0;
+          z-index: 999;
+      }
+  }
+</style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
+  <h2>test_homeview</h2>
   <main>
     <TheWelcome />
   </main>
